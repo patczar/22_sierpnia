6eab0acfd00e40d860f4fccb9d51d0e85fcc9fb3
Patryk Czarnik <patryk@patrykczarnik.net>
2021-08-22 11:22:02 +0200

    Pytanie o wiek

--- src/hello/Rozmowa.java
+++ src/hello/Rozmowa.java
@@ -7,6 +7,12 @@ public class Rozmowa {
 	public static void main(String[] args) {
 		String imie = JOptionPane.showInputDialog("Jak masz na imię?");
 		JOptionPane.showMessageDialog(null, "Witaj " + imie);
+		int wiek = Integer.parseInt(JOptionPane.showInputDialog("Ile masz lat?"));
+		if(wiek >= 18) {
+			JOptionPane.showMessageDialog(null, "Chodźmy na piwo");
+		} else {
+			JOptionPane.showMessageDialog(null, "Może lemoniadę?");
+		}
 	}
 
 }
