ed4ca68ed68ef70f9e2bc9043b65139ce0684805
Patryk Czarnik <patryk@patrykczarnik.net>
2021-08-22 11:39:00 +0200

    ostatnia zmiana

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Pełna informacdja o dacie i czasie: " + ZonedDateTime.now());
+		System.out.println("Do widzenia");
 	}
 
 }
