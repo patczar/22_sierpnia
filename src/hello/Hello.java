6ca5932ac53ee83e44276b9180703d3102c20f2a
Patryk Czarnik <patryk@patrykczarnik.net>
2021-08-22 10:21:31 +0200

    Odczyt także daty

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Hello {
@@ -7,6 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Teraz jest godzina " + LocalTime.now());
+		System.out.println("A tak w ogóle, to dzisiaj jest " + LocalDate.now());
 	}
 
 }
